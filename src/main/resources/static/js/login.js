ec89d5b99d3064b7b7706429ed5a9d6964343bb1
lkonaite <lkonaite@gmail.com>
2015-11-17 16:17:44 +0200

    Latest update

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,60 +1,28 @@
-var login = angular.module('login', ['ngRoute']);
+var eventsupdates = angular.module('eventsupdates', ['ngRoute']);
 
-login.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-                .when('/login', {
-                    'templateUrl': '/html/loginForm.html',
-                    'controller': 'loginCtrl'
-                })
-                .when('/user', {
-                    'templateUrl': '/html/users.html',
-                    'controller': 'userCtrl'
-                })
-                .otherwise({
-                    redirectTo: '/login'
-                });
-    }]);
-
-login.controller('loginCtrl', function ($scope, $http, $location) {
-
-    $scope.modelData = {};
-
-    $scope.submitLogin = function (loginform) {
-
-        $scope.modelData.user_Name = $scope.userName;
-        $scope.modelData.password = $scope.password;
-        $scope.modelData.role = $scope.role;
-
-        if (loginform.$invalid) {
-            console.log("Form Validation is Failled");
-        } else {
-            console.log("Form Validation is Successfull");
-
-            $http({
-                url: '/api/createuser',
-                method: 'post',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                data: $scope.modelData
-            }).success(function (data, status) {
-                if (status == 200) {
-                    console.log('user creation invoked successfully');
-                    console.log('Data:' + data);
-                    $location.path("/users");
-                } else {
-                    console.log('status:' + status);
-                }
-            })
-                    .error(function (error) {
-                        console.log(error);
-                    });
-        }
-    };
-});
+polygon.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+	.when('/login', {
+		'templateUrl': '/html/login.html',
+		'controller': 'loginCtrl'
+	}).otherwise({
+		redirectTo: '/home'
+	});
+}]);
 
 
+eventsupdates.directive('fileModel', ['$parse', function ($parse) {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attrs) {
+			var model = $parse(attrs.fileModel);
+			var modelSetter = model.assign;
 
-
-
-
+			element.bind('change', function () {
+				scope.$apply(function () {
+					modelSetter(scope, element[0].files[0]);
+				});
+			});
+		}
+	};
+}]);
